69e7fa52b5e2b0902743e418edd132d34a7bd628
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-13 15:29:25 -0500

    updated unit Test

--- FirstUnitTest/UnitTest1.cs
+++ FirstUnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace FirstUnitTest
 {
     public class UnitTest1
@@ -6,17 +8,56 @@ namespace FirstUnitTest
         public void PassingCaseTest()
         {
             //Arrange
-            int Year = 2022;
-            int birthYear = 1986;
-
-            int expected = 36;
+            string name = "Victor";
+            int birthYear = 1983;
             int actual;
-
+            int expected = 39;
+            UserAgeModel ageModel = new UserAgeModel();
+            ageModel.birthYear = birthYear;
+            ageModel.Name = name;
             //Act
-            actual = FirstResponsiveWebAppChristensen.Models.UserAgeModel.AgeThisYear();
-
+            actual = ageModel.AgeThisYear();
             //Assert
             Assert.Equal(expected, actual);
         }
     }
+
+    public class UnitTest2
+    {
+        public void PassingCaseTest()
+        {
+            //Arrange
+            string name = "Dave";
+            int birthYear = 2015;
+            int actual;
+            int expected = 7;
+            UserAgeModel ageModel = new UserAgeModel();
+            ageModel.birthYear = birthYear;
+            ageModel.Name = name;
+            //Act
+            actual = ageModel.AgeThisYear();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class UnitTest3
+    {
+        public void PassingCaseTest2()
+        {
+            //Arrange
+            string name = "Ferdinand";
+            int birthYear = 1923;
+            int actual;
+            int expected = 99;
+            UserAgeModel ageModel = new UserAgeModel();
+            ageModel.birthYear = birthYear;
+            ageModel.Name = name;
+            //Act
+            actual = ageModel.AgeThisYear();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+
 }
